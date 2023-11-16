3ec89021fb63d5726375a36150dd9079c5e0c64f
srikrishnadinda018 <127649098+srikrishnadinda018@users.noreply.github.com>
2023-11-16 10:14:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const getChatResponse = async (incomingChatDiv) => {
         body: JSON.stringify({
             model: "text-davinci-003",
             prompt: userText,
-            max_tokens: 2048,
+            max_tokens: 1024,
             temperature: 0.2,
             n: 1,
             stop: null
@@ -72,7 +72,7 @@ const showTypingAnimation = () => {
                             <div class="typing-dot" style="--delay: 0.4s"></div>
                         </div>
                     </div>
-                    
+                    <button class="more-info-btn" id="more-info"><span>more info</span></button>
                 </div>`;
     // Create an incoming chat div with typing animation and append it to chat container
     const incomingChatDiv = createChatElement(html, "incoming");
