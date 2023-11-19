5c0dca561538c91ade8f8aa4009a52854e40aa8f
srikrishnadinda018 <127649098+srikrishnadinda018@users.noreply.github.com>
2023-11-19 17:22:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,122 @@
+const chatInput = document.querySelector("#chat-input");
+const sendButton = document.querySelector("#send-btn");
+const chatContainer = document.querySelector(".chat-container");
+const deleteButton = document.querySelector("#delete-btn");
+const search = document.querySelector("#search-btn");
+const translation = document.querySelector("#translation-btn");
 
+let userText = null;
+const API_KEY = "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"; // Paste your API key here
+
+const createChatElement = (content, className) => {
+    const chatDiv = document.createElement("div");
+    chatDiv.classList.add("chat", className);
+    chatDiv.innerHTML = content;
+    return chatDiv;
+}
+
+const getChatResponse = async (incomingChatDiv) => {
+    const API_URL = "https://api.openai.com/v1/completions";
+    const pElement = document.createElement("p");
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "text-davinci-003",
+            prompt: userText,
+            max_tokens: 2048,
+            temperature: 0.2,
+            n: 1,
+            stop: null
+        })
+    }
+
+    try {
+        const response = await fetch(API_URL, requestOptions);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const responseData = await response.json();
+        const chatText = responseData.choices[0]?.text?.trim() || "No response from the API.";
+
+        pElement.textContent = chatText;
+    } catch (error) {
+        pElement.classList.add("error");
+        pElement.textContent = "Oops! Something went wrong while retrieving the response. Please try again.";
+    }
+
+    incomingChatDiv.querySelector(".typing-animation").remove();
+    incomingChatDiv.querySelector(".chat-details").appendChild(pElement);
+
+    const utterance = new SpeechSynthesisUtterance(pElement.textContent);
+    speechSynthesis.speak(utterance);
+
+    translation.addEventListener("click", () => {
+        const googleTranslateUrl = `https://translate.google.com/?sl=en&tl=bn&text=${encodeURIComponent(pElement.textContent)}`;
+        window.open(googleTranslateUrl, '_blank');
+    });
+
+    chatContainer.scrollTo(0, chatContainer.scrollHeight);
+}
+
+const showTypingAnimation = () => {
+    const html = `<div class="chat-content">
+                    <div class="chat-details">
+                        <img src="static folder/bot.png" alt="chatbot-img">
+                        <div class="typing-animation">
+                            <div class="typing-dot" style="--delay: 0.2s"></div>
+                            <div class="typing-dot" style="--delay: 0.3s"></div>
+                            <div class="typing-dot" style="--delay: 0.4s"></div>
+                        </div>
+                    </div>
+                </div>`;
+    const incomingChatDiv = createChatElement(html, "incoming");
+    chatContainer.appendChild(incomingChatDiv);
+    chatContainer.scrollTo(0, chatContainer.scrollHeight);
+    getChatResponse(incomingChatDiv);
+}
+
+const handleOutgoingChat = () => {
+    userText = chatInput.value.trim();
+    if (!userText) return;
+
+    chatInput.value = "";
+    chatInput.style.height = `${initialInputHeight}px`;
+
+    const html = `<div class="chat-content">
+                    <div class="chat-details">
+                        <img src="static folder/user.jpg" alt="user-img">
+                        <p>${userText}</p>
+                    </div>
+                </div>`;
+
+    const outgoingChatDiv = createChatElement(html, "outgoing");
+    chatContainer.appendChild(outgoingChatDiv);
+    chatContainer.scrollTo(0, chatContainer.scrollHeight);
+    setTimeout(showTypingAnimation, 500);
+}
+
+deleteButton.addEventListener("click", () => {
+    if (confirm("Are you sure you want to delete all the chats?")) {
+        window.location.reload();
+    }
+});
+
+search.addEventListener("click", () => {
+    const googleURL = `https://www.google.com/search?q=${userText}`;
+    window.open(googleURL, '_blank');
+});
+
+const initialInputHeight = chatInput.scrollHeight;
+
+chatInput.addEventListener("input", () => {
+    chatInput.style.height = `${initialInputHeight}px`;
+    chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+
+sendButton.addEventListener("click", handleOutgoingChat);
